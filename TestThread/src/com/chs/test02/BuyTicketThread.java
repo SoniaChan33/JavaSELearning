62e355941f840dec6197472c4a6943f71e388031
SoniaChan33 <soniachan33@126.com>
2021-07-17 15:50:17 +0800

    JAVA SE 笔记的第一次提交

---
+++ TestThread/src/com/chs/test02/BuyTicketThread.java
@@ -0,0 +1,29 @@
+package com.chs.test02;
+
+/**
+ * @Auther:ChanHuiShan
+ * @Date: 2021-04-30 - 04 - 30 - 10:38
+ * @Description: com.chs.test02
+ * @version: 1.0
+ */
+public class BuyTicketThread implements Runnable{
+    /*static */int tickNum = 10;//这里的三个窗口都是使用的这一个对象，不需要使用静态
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            buyTicket();
+        }
+    }
+
+
+    public synchronized void buyTicket(){//锁住的方法就是锁住了方法这个对象this
+        if(tickNum>0){
+            System.out.println("我在"+Thread.currentThread().getName()+"买到了北京到哈尔滨的第"+tickNum--+"张车票");
+        }
+    }
+
+
+
+
+
+}
